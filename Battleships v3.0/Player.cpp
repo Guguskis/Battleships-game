a376e8dacd006090bd2b005ba9631effc29b6489
Matas Liutikas <matasliutikas@gmail.com>
2018-09-20 19:13:04 +0300

    Graphics update

--- Battleships v3.0/Player.cpp
+++ Battleships v3.0/Player.cpp
@@ -4,14 +4,24 @@
 int Player::counter = 0;
 
 Player::Player() {
+	//initializing variables and arrays
 	counter++;
+	srand(time(NULL) + counter);
 	mMessage = "";
 	mWidth = mLength = 10;
 	vector<int> tempLength;
-	for (int i = 0; i<mLength; i++)
+	vector<ColorTile> tempTileLength;
+	ColorTile tempTile;
+	tempTile.color = 0;
+	tempTile.calculated = false;
+	for (int i = 0; i < mLength; i++) {
 		tempLength.push_back(0);
-	for (int i = 0; i<mWidth; i++)
+		tempTileLength.push_back(tempTile);
+	}
+	for (int i = 0; i < mWidth; i++) {
 		mBoard.push_back(tempLength);
+		mTiles.push_back(tempTileLength);
+	}
 	//generating ships
 	ships.push_back(4);
 	for (int i = 0; i<2; i++)
@@ -22,6 +32,8 @@ Player::Player() {
 		ships.push_back(1);
 	PlaceShipsOnBoard();
 	mShipCounter = ships.size();
+	//generating color board
+	GenerateColorBoard();
 }
 Player::Player(int width, int length) {
 	counter++;
@@ -45,48 +57,70 @@ Player::Player(int width, int length) {
 	mShipCounter = ships.size();
 }
 void Player::DrawYourBoard() {
-	cout << "   ";
+	cout << "              ";
 	for (int i = 0; i < mLength; i++)
 		cout << i;
 	cout << endl;
 	for (int i = 0; i<mWidth; i++) {
-		cout << " " << (char)('A' + i) << " ";
+		SetColor("black", 0);
+		cout << "            " << (char)('A' + i) << " ";
 		for (int j = 0; j<mLength; j++) {
 			switch (mBoard[i][j]) {
-			case 0: cout << "~"; break;
-			case 1: cout << "O"; break;
-			case 2: cout << "X"; break;
-			case 3: cout << "."; break;
+			case 0: 
+				SetColor("water", mTiles[i][j].color);
+				cout << "~"; break;
+			case 1: 
+				SetColor("ship", mTiles[i][j].color);
+				cout << "#"; break;
+			case 2: 
+				SetColor("shipDestroyed", mTiles[i][j].color);
+				cout << "X"; break;
+			case 3: 
+				SetColor("missed", mTiles[i][j].color);
+				cout << "+"; break;
 			default: cout << "Tile not specified in Player.DrawBoardAll();"; break;
 			}
 		}
+		SetColor("black", 0);
 		cout << " " << (char)('A'+i) << endl;
 	}
-	cout << "   ";
+	SetColor("black", 0);
+	cout << "              ";
 	for (int i = 0; i < mLength; i++)
 		cout << i;
 	cout << endl;
 }
 void Player::DrawEnemyBoard() {
 	//TODO: add coordinates for shooting guidance
-	cout << "   ";
+	cout << "              ";
 	for (int i = 0; i < mLength; i++)
 		cout << i;
 	cout << endl;
-	for (int i = 0; i<mWidth; i++) {
-		cout << " " << (char)('A' + i) << " ";
-		for (int j = 0; j<mLength; j++) {
+	for (int i = 0; i < mWidth; i++) {
+		SetColor("black", 0);
+		cout << "            " << (char)('A' + i) << " ";
+		for (int j = 0; j < mLength; j++) {
 			switch (mBoard[i][j]) {
-			case 0: cout << "~"; break;
-			case 1: cout << "~"; break;
-			case 2: cout << "X"; break;
-			case 3: cout << "."; break;
+			case 0:
+				SetColor("water", mTiles[i][j].color);
+				cout << "~"; break;
+			case 1:
+				SetColor("water", mTiles[i][j].color);
+				cout << "~"; break;
+			case 2:
+				SetColor("shipDestroyed", mTiles[i][j].color);
+				cout << "X"; break;
+			case 3:
+				SetColor("missed", mTiles[i][j].color);
+				cout << "+"; break;
 			default: cout << "Tile not specified in Player.DrawBoardAll();"; break;
 			}
 		}
+		SetColor("black", 0);
 		cout << " " << (char)('A' + i) << endl;
 	}
-	cout << "   ";
+	SetColor("black", 0);
+	cout << "              ";
 	for (int i = 0; i < mLength; i++)
 		cout << i;
 	cout << endl;
@@ -120,7 +154,6 @@ bool Player::CanShipBePlaced(Ship ship) {
 	return true;
 }
 void Player::PlaceShipsOnBoard() {
-	srand(time(NULL) + counter);
 	for (int i = 0; i<ships.size(); i++) {
 		//randomizing coordinates until ship can be placed
 		do {
@@ -166,7 +199,7 @@ bool Player::CheckIfShipIsDestroyed(Ship ship) {
 			if (mBoard[ship.YFront][i] != 2)
 				return false;
 	}
-	//vertica
+	//vertical
 	else{
 		for (int i = ship.YFront; i < ship.YFront + ship.Length; i++)
 			if (mBoard[i][ship.XFront] != 2)
@@ -299,6 +332,182 @@ int Player::GetWidth() const{
 int Player::GetLength() const{
 	return mLength;
 }
-void Player::SetColor(int color) {
-	//to finish
+void Player::SetColor(string object, int color) {
+	int bgValue=0, charValue=0;
+
+	/*
+		Blue color values (from darkest to lightest):
+			15 - dark blue
+			143 - light blue
+			47 - cyan
+			175 - light cyan
+
+	*/
+
+	if (object != "black") {
+		//setting background values
+		switch (color) {
+		case 0: bgValue = 15; break;
+		case 1: bgValue = 15; break;
+		case 2: bgValue = 15; break;
+		case 3: bgValue = 143; break;
+		case 4: bgValue = 143; break;
+		case 5: bgValue = 143; break;
+		case 6: bgValue = 47; break;
+		}
+
+		//setting character values
+		if(object=="water"){
+			switch (color) {
+			case 0: charValue = 2; break;
+			case 1: charValue = 10; break;
+			case 2: charValue = 2; break;
+			case 3: charValue = 10; break;
+			case 4: charValue = 12; break;
+			case 5: charValue = 10; break;
+			case 6: charValue = 12; break;
+			}
+		}
+		else if (object == "ship") charValue = 1;
+		else if (object == "shipDestroyed") {
+			charValue = 5;
+			//bgValue = 191;
+		}
+		else if (object == "missed") charValue = 16;
+	}
+	else {
+		bgValue = 0;
+		charValue = 7;
+	}
+
+	HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hcon, bgValue+charValue);
+}
+void Player::GenerateColorBoard() {
+	  
+	/*
+		placing random seeds
+	*/
+	int tempY, tempX;
+	for (int i = 0; i < mColorSeeds; i++) {
+		int y, x, tempColor;
+		tempColor = rand() % (mColorMax - mColorMin + 1) + mColorMin;
+		do {
+			//substracting 1 avoid calling recursion out of bounds
+			y = rand() % (mWidth-1);
+			x = rand() % mLength;
+		} while (mTiles[y][x].calculated);
+
+		mTiles[y][x].color = tempColor;
+		mTiles[y][x].calculated = true;
+		int tempDepth = 0;
+		GenerateColorBoardRecursion(y+1, x, tempDepth);
+	}
+
+	/*
+		filling remaining board
+	*/
+	for (int i = 0; i < mWidth; i++) {
+		for (int j = 0; j < mLength; j++) {
+			int tempDepth = 0;
+			GenerateColorBoardRecursion(i, j, tempDepth);
+		}
+	}
+	/*
+		normalizing values and randomizing colors that are between two distic
+	*/
+	for (int i = 0; i < mWidth; i++) {
+		for (int j = 0; j < mLength; j++) {
+			mTiles[i][j].color = RandomizeColorTranzitions(mTiles[i][j].color);
+		}
+	}
+}
+void Player::GenerateColorBoardRecursion(int y, int x, int &depth) {
+	/*
+		depth parameter prevents single seed domination
+	*/
+	if (y >= 0 && y < mWidth && x >= 0 && x < mLength && depth < recursionDepth) {
+		depth++;
+		if (!mTiles[y][x].calculated) {
+			mTiles[y][x].color = SurroundingTilesAverage(y, x);
+			int temp;
+			do {
+				/*
+					making change less frequent
+				*/
+				temp = rand() % 5;
+				switch (temp) {
+					case 0: temp = -1; break;
+					case 4: temp = 1; break;
+					default: temp = 0; break;
+				}
+			} while (!(mTiles[y][x].color + temp >= mColorMin && mTiles[y][x].color + temp <= mColorMax));
+			mTiles[y][x].color += temp;
+			mTiles[y][x].calculated = true;
+
+			/*
+				randomizing the direction of recursion
+			*/
+			vector<int> order;
+			for (int i = 0; i < 4; i++)
+				order.push_back(i);
+
+			random_shuffle(order.begin(), order.end());
+			for (int i = 0; i < 4; i++) {
+				switch (i) {
+					case 0: this->GenerateColorBoardRecursion(y - 1, x, depth); break;
+					case 1: this->GenerateColorBoardRecursion(y, x + 1, depth); break;
+					case 2: this->GenerateColorBoardRecursion(y + 1, x, depth); break;
+					case 3: this->GenerateColorBoardRecursion(y, x - 1, depth); break;
+					default: break;
+				}
+			}
+		}
+	}
+}
+int Player::SurroundingTilesAverage(int y, int x) {
+	//calculates the average of 3x3 area 
+	int sum = 0, tileCount = 0;
+	for (int i = y - 1; i <= y + 1; i++) {
+		for (int j = x - 1; j <= x + 1; j++) {
+			if (j >= 0 && j < mWidth && i >= 0 && i < mLength) {
+				if (mTiles[i][j].calculated) {
+					sum += mTiles[i][j].color;
+					tileCount++;
+				}
+			}
+		}
+	}
+	//for some reason at element [0][0] it counts zero tiles after filling remaining board in GenerateColorBoard
+	if(tileCount!=0) return round(sum*1.0 / tileCount);
+	else return round((1.0*mColorMax - mColorMin) / 2);
+}
+void Player::DEBUGTILES() {
+	for (int i = 0; i < mWidth; i++) {
+		for (int j = 0; j < mLength; j++) {
+			cout << mTiles[i][j].color;
+		}
+		cout << endl;
+	}
+}
+int Player::RandomizeColorTranzitions(int color) {
+	int temp = rand() % 2;
+	switch (color) {
+	case 0: return 0; 
+	case 1: return 1;
+	case 2: 
+		if (temp == 0) return 1;
+		else return 2;
+	case 3: return 2;
+	case 4: return 3;
+	case 5: return 4;
+	case 6:
+		if (temp == 0) return 4;
+		else return 5;
+	case 7: return 5;
+	case 8: return 6;
+	default:
+		cout << "\nColor value " << color << " not specified in RandomizeColorTranzitions function. Returning -1\n";
+		return -1;
+	}
 }
--- Battleships v3.0/Player.h
+++ Battleships v3.0/Player.h
@@ -6,6 +6,8 @@
 #include <string>
 #include <cmath>
 #include "Ship.h"
+#include <Windows.h>
+#include <algorithm>
 using namespace std;
 
 #ifndef PLAYER_H
@@ -30,18 +32,33 @@ public:
 	bool CheckIfLost();
 	int GetWidth() const;
 	int GetLength() const;
+	void DEBUGTILES();
 private:
 	int mLength, mWidth, mShipCounter;
+	/*
+		recommendations for tweaking these settings:
+			mColorSeeds*recursionDepths>=(mWidth*mLength)/2
+	*/
+	int mColorSeeds=10, mColorMin = 0, mColorMax = 8, recursionDepth=10;
 	static int counter;
 	string mMessage;
 	vector<Ship> ships;
+	struct ColorTile {
+		int color;
+		bool calculated;
+	};
+	vector<vector<ColorTile> > mTiles;
 	//	-	-	-	-	-
 	bool CanShipBePlaced(Ship);
 	void PlaceShipsOnBoard();
-	void SetColor(int);
+	void SetColor(string, int);
 	bool CheckIfShipIsDestroyed(Ship);
 	void RevealShipsIfDestroyed();
 	pair<int, int> StringToIntCoordsConverter(string);
+	void GenerateColorBoard();
+	void GenerateColorBoardRecursion(int, int, int&);
+	int SurroundingTilesAverage(int, int);
+	int RandomizeColorTranzitions(int);
 };
 
 #endif
